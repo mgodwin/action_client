1b943d179707f66c62e4d7a5c452aec7935b539c
Mark Godwin <godwin.mark@gmail.com>
2023-01-01 19:08:01 +0000

    initial commit

---
+++ lib/action_client.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "action_client/version"
+
+module ActionClient
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/action_client/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ActionClient
+  VERSION = "0.1.0"
+end
